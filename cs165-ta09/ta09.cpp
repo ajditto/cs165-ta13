c9a7d5ceabaff2010356122fd59c014cfa2e7ede
andrew james ditto <dittoja@LinuxLab05.byui.edu>
2017-03-01 19:04:30 -0700

    header for ta09

---
+++ cs165-ta09/ta09.cpp
@@ -0,0 +1,12 @@
+/******************************************************************************
+ * Assignment:
+ *    Team Acitivity 09, Polyorphism
+ * Authors
+ *    Andrew Ditto and Dylan Bullock
+ * Summary
+ *    Fufulling the requirements for ta09
+ *****************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
