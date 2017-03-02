7afde0859850c087425de863579793a2abbc0f20
andrew james ditto <dittoja@LinuxLab05.byui.edu>
2017-03-01 19:09:08 -0700

    created Employee class

--- cs165-ta09/ta09.cpp
+++ cs165-ta09/ta09.cpp
@@ -10,3 +10,12 @@
 #include <string>
 using namespace std;
 
+class Employee {
+   private: 
+      string name;
+
+   public:
+      string getName() { return name; }
+      void setName(string name) { this -> name = name; }
+      void display() = 0;
+}
