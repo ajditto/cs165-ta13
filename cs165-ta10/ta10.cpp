b1a6bed5aa137ba0682076a0b3c318f1770c245d
andrew james ditto <dittoja@LinuxLab05.byui.edu>
2017-03-08 13:06:34 -0700

    Creation of TA10 file

---
+++ cs165-ta10/ta10.cpp
@@ -0,0 +1,9 @@
+/******************************************************************************
+ * Assignment:
+ * 	Team Activity 10, STL and iterators overview
+ * Professor:
+ * 	Bro. McCracken, CS165
+ * Authors:
+ * 	Andrew Ditto and Dylan Bullock
+ *****************************************************************************/
+
